89961fdf2d1a4d4e68a0e51c70a21e5078d1a88e
Nonone <louis.josephderien@gmail.com>
2021-09-17 01:40:07 +0200

    ajout projet 🚀

--- projects.php
+++ projects.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
@@ -10,51 +10,63 @@
 </head>
 
 <body>
+    <div class="wrapper">
+        <header>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="index.php">À propos</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="skills.php">Mes compétences</a></li>
+                    <li class="btn"><a href="./Espace admin/login.php">Admin</a></li>
+                </ul>
+            </div>
+        </header>
+    </div>
     <section class="sub-section" id="about-me">
         <div class="information">
             <h2>Mes engagements</h2>
             <p>Utiliser un maximum les verbes qui finissent par "endre" car ce sont les plus utiles de la langue française : Entendre. Comprendre. Apprendre. Entreprendre. Surprendre..</p>
         </div>
         <div class="headshot-container">
-            <img class="headshot" src="" alt="" />
+            <img class="headshot" src="img/a propos.png" alt="illustration engagements" />
         </div>
-        <section class="sub-section-alternative" id="projects">
-            <h2>Ce que l’on peut faire ensemble</h2>
-            <p>Cette page ne sert qu’à une seule chose : vous raconter ce que je peut faire pour vous.</br>
-                Plus que mes compétences, ces 4 items représentent ce que j'ai fait en formation et ce sur quoi je continue de m'améliorer tous les jours.</p>
-            <div class="project-container">
-                <div class="project-card">
-                    <img class="project-image" src="src/images/Webdesign.png" alt="Project One Image" />
-                    <h3>Psd to Html/Css</h3>
-                    <p class="subtext">Intégrer une maquette en html/css. Projet en collaboration.</p>
-                    <hr />
-                    <p class="subtext"><a class="project-link" href="https://github.com/Louis-joseph/Psd-to-html_css" target="_blank">cliquez ici.</a></p>
-                </div>
-                <div class="project-card">
-                    <img class="project-image" src="src/images/Outillage.png" alt="Project One Image" />
-                    <h3>PHP/MySQL/Js</h3>
-                    <p class="subtext">Dashboard gestion d'ampoules d'un immeubles.</p>
-                    <hr />
-                    <p class="subtext"><a class="project-link" href="https://github.com/Louis-joseph/Projet_ampoule/tree/groowww" target="_blank">cliquez ici.</a></p>
-                </div>
-                <div class="project-card">
-                    <img class="project-image" src="src/images/Mise+en+relation.png" alt="Project One Image" />
-                    <h3>PHP/POO/MCD</h3>
-                    <p class="subtext">Plateforme de réservation de gîtes.</p>
-                    <hr />
-                    <p class="subtext"><a class="project-link" href="https://www.github.com/kubowania" target="_blank">cliquez ici.</a></p>
-                </div>
-                <div class="project-card">
-                    <img class="project-image" src="src/images/Copywriting.png" alt="Project One Image" />
-                    <h3>PHP/MySQL/Wordpress</h3>
-                    <p class="subtext">Réalisation du Blog de la promotion les Bots de Nevers 2021.</p>
-                    <hr />
-                    <p class="subtext"><a class="project-link" href="https://www.github.com/kubowania">cliquez ici.</a></p>
-                </div>
-            </div>
-        </section>
     </section>
-
+    <section class="sub-section-alternative" id="projects">
+        <h2>Ce que l’on peut faire ensemble</h2>
+        <p>Cette page ne sert qu’à une seule chose : vous raconter ce que je peut faire pour vous.</br>
+            Plus que mes compétences, ces 4 items représentent ce que j'ai fait en formation et ce sur quoi je continue de m'améliorer tous les jours.</p>
+        <div class="project-container">
+            <div class="project-card">
+                <img class="project-image" src="img/Webdesign.png" alt="Project One Image" />
+                <h3>Psd to Html/Css</h3>
+                <p class="subtext">Intégrer une maquette en html/css. Projet en collaboration.</p>
+                <hr />
+                <p class="subtext"><a class="project-link" href="https://github.com/Louis-joseph/Psd-to-html_css" target="_blank">Cliquez ici</a></p>
+            </div>
+            <div class="project-card">
+                <img class="project-image" src="img/Outillage.png" alt="Project One Image" />
+                <h3>PHP/MySQL/Js</h3>
+                <p class="subtext">Dashboard gestion d'ampoules d'un immeubles.</p>
+                <hr />
+                <p class="subtext"><a class="project-link" href="https://github.com/Louis-joseph/Projet_ampoule/tree/groowww" target="_blank">Cliquez ici</a></p>
+            </div>
+            <div class="project-card">
+                <img class="project-image" src="img/Mise+en+relation.png" alt="Project One Image" />
+                <h3>PHP/POO/MCD</h3>
+                <p class="subtext">Plateforme de réservation de gîtes. Projet en collaboration</p>
+                <hr />
+                <p class="subtext"><a class="project-link" href="https://github.com/Louis-joseph/Stackoo" target="_blank">Cliquez ici</a></p>
+            </div>
+            <div class="project-card">
+                <img class="project-image" src="img/Copywriting.png" alt="Project One Image" />
+                <h3>PHP/MySQL/Wordpress</h3>
+                <p class="subtext">Réalisation du Blog de la promotion les Bots de Nevers 2021.</p>
+                <hr />
+                <p class="subtext"><a class="project-link" href="">Cliquez ici</a></p>
+            </div>
+        </div>
+    </section>
 </body>
 
 </html>
